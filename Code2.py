f5d1c9ea22b8821e87d823db8ea7a26770c63ce1
mohammad <okmo8578@gmail.com>
2024-11-05 14:14:54 +0330

    code2 review

--- Code2.py
+++ Code2.py
@@ -1,5 +1,20 @@
-def fun():
-    print("Welcome to GG")
+import tkinter as tk
 
+window = tk.Tk()
+window.geometry("500x500+100+100")
 
-fun()
+def evenOdd(x):
+    if (x % 2 == 0):
+        return 2
+    else:
+        return 1
+    
+result1 = evenOdd(2)
+result2 = evenOdd(3)
+
+input1 = tk.Entry(textvariable="mo")
+input1.pack()
+
+print(result1)
+
+tk.mainloop()
