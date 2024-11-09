38075469b76ca830fef873e8493ad728ee0415d0
mohammad <okmo8578@gmail.com>
2024-11-09 16:08:05 +0330

    a

--- skyfield.py
+++ skyfield.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.messagebox as msg
 
 window_1 = tk.Tk()
-window_1.geometry("500x500")
+window_1.geometry("500x700")
 
 class window():
     def __init__(self, height, width) -> None:
@@ -19,5 +19,19 @@ text2.pack()
 text3 = tk.Label(window_1, text="hello", font="./font/sylfaen.ttf")
 text3.pack()
 
-tk.mainloop()
+text4 = tk.Button(window_1, text="Hello", font="./font/sylfaen.ttf", bg="cyan")
+text4.pack()
 
+text5 = tk.Scrollbar(window_1)
+text5.pack()
+
+text6 = tk.Listbox(window_1, bg="cyan")
+text6.pack()
+
+text7 = tk.Checkbutton(window_1)
+text7.pack()
+
+text8 = tk.Checkbutton(window_1, text="A", font="Bold")
+text8.pack()
+
+tk.mainloop()
